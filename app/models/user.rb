0d3da076027ef6a5100f08c0ae90024bb64a0262
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-08-13 19:28:45 -0300

    Add the owner, bike relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bikes, through: :rents
   has_many :rents
+  has_many :bikes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
