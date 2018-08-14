e019f7385961e79e4b565d53a7ccef0fb8bb10f5
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-08-14 11:33:23 -0300

    Add some validations

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,4 +2,5 @@ class Bike < ApplicationRecord
   has_many :rents
   belongs_to :user
   has_many :users, through: :rents
+  validates :kind, :brand, :location, :size, :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  # user database relations
   has_many :bikes, through: :rents
   has_many :rents
   has_many :bikes
+  # validations of user beyond basic devise
+  validates :first_name, :last_name, :location, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
