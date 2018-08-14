616cd9d81d735d9d796faa1fe4f831d588233fe2
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-08-13 21:33:50 -0300

    adding some comments and thoughts

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,5 @@
 class Bike < ApplicationRecord
+  # the ActiveRecord routing for our database connections
   has_many :rents
   belongs_to :user
   has_many :users, through: :rents
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  # root to: 'pages#home'
+  # added the basic root
   root to: 'bikes#index'
+  # creates the routes we need for bikes and the nested rents routes i believe were needed
   resources :bikes do
     resources :rents, only: :new
   end
+  # creates some of the other rents routes we might need but need to verify
   resources :rents
 end
