ba0aa2e69a1b664248afc809eda99a61dff2f696
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-08-14 13:27:36 -0300

    Add the dependent destroy to user and removed brand validation on bikes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # user database relations
   has_many :bikes, through: :rents
   has_many :rents
-  has_many :bikes
+  has_many :bikes, dependent: :destroy
   # validations of user beyond basic devise
-  validates :first_name, :last_name, :location, presence: true
+  validates :first_name, :location, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
