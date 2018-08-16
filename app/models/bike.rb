c1264a6b9a8059d3d0cf24f9d4c5670f48023b9b
Michael Resnick <michaeln.resnick@gmail.com>
2018-08-16 18:30:09 -0300

    nothing here

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,5 +1,5 @@
 class Bike < ApplicationRecord
-  mount_uploader :photo, PhotoUploader
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many :rents, dependent: :destroy
