e48ce6dba0dda2bfeddc6d21afb1347761a32034
Michael Resnick <michaeln.resnick@gmail.com>
2018-08-16 16:35:38 -0300

    more things

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  has_many :rents
+  has_many :rents, dependent: :destroy
   belongs_to :user
   has_many :users, through: :rents
   validates :kind, :address, :size, :price, presence: true
